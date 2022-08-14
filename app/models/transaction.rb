0cbdaec1356380ddb4c41a18e0a8da7a5a96c554
Enoch Otieno <enochotieno@rocketmail.com>
2022-08-14 21:38:17 +0300

    Added the test examples for models with all tests succeeding

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,7 @@
 class Transaction < ApplicationRecord
+    validates_presence_of :input_amount
+    validates_presence_of :input_currency
+    validates_presence_of :output_currency
+    validates_presence_of :customer_id
+    validates_presence_of :date_of_transaction
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  subject {described_class.new(
+    input_amount: 100,
+    input_currency: "USD",
+    output_currency: "KES",
+    date_of_transaction: DateTime.now,
+    customer_id: 1
+  )}
+  it "is valid with valid attributes" do 
+    expect(subject).to be_valid
+  end
+  it "is not valid without an input currency" do
+    subject.input_currency = nil
+    expect(subject).to_not be_valid
+  end
+  it "is not valid without an input amount" do
+    subject.input_amount = nil
+    expect(subject).to_not be_valid
+  end
+  it "is not valid without an output currency" do
+    subject.output_currency = nil
+    expect(subject).to_not be_valid
+  end
+  it "is not valid without a customer id" do
+    subject.customer_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a date of transaction" do
+    subject.date_of_transaction = nil
+    expect(subject).to_not be_valid
+  end
 end
