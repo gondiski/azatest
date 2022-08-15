cc8ea1357b8ee5feb57d55b0400093cab1d11e8d
Enoch Otieno <enochotieno@rocketmail.com>
2022-08-15 08:36:14 +0300

    Adjustment to display only required endpoints

--- spec/requests/api/v1/transactions_spec.rb
+++ spec/requests/api/v1/transactions_spec.rb
@@ -69,49 +69,49 @@ RSpec.describe 'api/v1/transactions', type: :request do
       end
     end
 
-    patch('update transaction') do
-      response(200, 'successful') do
-        let(:id) { '123' }
+    # patch('update transaction') do
+    #   response(200, 'successful') do
+    #     let(:id) { '123' }
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
+    #     after do |example|
+    #       example.metadata[:response][:content] = {
+    #         'application/json' => {
+    #           example: JSON.parse(response.body, symbolize_names: true)
+    #         }
+    #       }
+    #     end
+    #     run_test!
+    #   end
+    # end
 
-    put('update transaction') do
-      response(200, 'successful') do
-        let(:id) { '123' }
+    # put('update transaction') do
+    #   response(200, 'successful') do
+    #     let(:id) { '123' }
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
+    #     after do |example|
+    #       example.metadata[:response][:content] = {
+    #         'application/json' => {
+    #           example: JSON.parse(response.body, symbolize_names: true)
+    #         }
+    #       }
+    #     end
+    #     run_test!
+    #   end
+    # end
 
-    delete('delete transaction') do
-      response(200, 'successful') do
-        let(:id) { '123' }
+    # delete('delete transaction') do
+    #   response(200, 'successful') do
+    #     let(:id) { '123' }
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
+    #     after do |example|
+    #       example.metadata[:response][:content] = {
+    #         'application/json' => {
+    #           example: JSON.parse(response.body, symbolize_names: true)
+    #         }
+    #       }
+    #     end
+    #     run_test!
+    #   end
+    # end
   end
 end
