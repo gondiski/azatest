40c83206b5d19c551310e53b3ced3d018d99fae0
Enoch Otieno <enochotieno@rocketmail.com>
2022-08-17 15:11:46 +0300

    Removed API key

--- lib/services/try.rb
+++ lib/services/try.rb
@@ -9,7 +9,7 @@ puts "Enter the amount:"
 input_amount = gets.chomp
 # Setting URL
 # url = "https://v6.exchangerate-api.com/v6/#{ENV['API_KEY']}/pair/#{input_currency}/#{output_currency}/#{input_amount}"
-url = "https://v6.exchangerate-api.com/v6/8017880d47879f56d184a0c0/pair/#{input_currency}/#{output_currency}/#{input_amount}"
+url = "https://v6.exchangerate-api.com/v6/#{ENV['API_KEY']}/pair/#{input_currency}/#{output_currency}/#{input_amount}"
 uri = URI(url)
 response = Net::HTTP.get(uri)
 response_obj = JSON.parse(response)
